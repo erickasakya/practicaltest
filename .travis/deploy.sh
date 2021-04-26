4ce26d0be4eb1cad28bd71f4085d05db124c4f5f
Eric Kasakya <erickasakya@gmail.com>
2021-04-26 19:17:52 +0300

    update script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -23,7 +23,7 @@ git push deploy master
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh travis@$IP -p $PORT <<EOF
     cd $DEPLOY_DIR
-    crystal build --release --no-debug index.cr # Change to whatever commands you need!
+    echo "Finished"
 EOF
   exit
 else
