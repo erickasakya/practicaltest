705584dc0d5704b8a97cf588b03f80c703d1404f
Eric Kasakya <erickasakya@gmail.com>
2021-04-27 15:13:34 +0300

    app port number

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    run_simple('127.0.0.1', 8000, app, use_debugger=True, use_reloader=True)
+    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
